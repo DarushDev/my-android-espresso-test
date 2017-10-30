c1c3265a1a1b7f648a17a9ebd6116a79f7171b8a
DarushDev <darushfaqiri@gmail.com>
2017-10-30 18:02:51 +0800

    Test if the SingleCoffeeActivity is launched when tapped on a coffee item and press back if succeeded

--- app/src/androidTest/java/com/example/myandroidespressotesting/CoffeeListActivityTest.java
+++ app/src/androidTest/java/com/example/myandroidespressotesting/CoffeeListActivityTest.java
@@ -1,8 +1,8 @@
 package com.example.myandroidespressotesting;
 
-import android.content.Context;
 import android.support.annotation.NonNull;
-import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.intent.Intents;
 import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -13,12 +13,17 @@ import android.view.View;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withChild;
@@ -27,13 +32,20 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
 public class CoffeeListActivityTest {
 
     @Rule
     public ActivityTestRule<ScrollingActivity> mActivityRule = new ActivityTestRule<>(ScrollingActivity.class);
+
+    @Before
+    public void setUp() throws Exception {
+        Intents.init();
+    }
+
+    /*@Rule
+    public IntentsTestRule<ScrollingActivity> intentsTestRule = new IntentsTestRule<>(ScrollingActivity.class);*/
 
     @Test
     public void testMapFloatingActionButton_shouldBeDisplayed() {
@@ -86,4 +98,34 @@ public class CoffeeListActivityTest {
             }
         };
     }
+
+    @Test
+    public void testBeverageClick_shouldOpenSingleCoffeeActivity() {
+
+        // This checks if the Cafe Bombon item is really not displayed on the screen
+        //onView(withText("Cafe Bombon")).check(matches(not(isDisplayed())));
+
+        // Scroll to position 7 (Last item) in recycler view
+        onView(withId(R.id.recycler_view))
+                .perform(RecyclerViewActions.scrollToPosition(7));
+
+        // Check if Cafe Bombon is displayed
+        onView(withText("Cafe Bombon")).check(matches(isDisplayed()));
+
+        // Perform a click action on recycler view item at position 6
+        onView(withId(R.id.recycler_view))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(6, click()));
+
+        // Indicates the intent that will open the SingleCoffeeActivity
+        intended(hasComponent(SingleCoffeeActivity.class.getName()));
+        //intending(toPackage("com.example.myandroidespressotesting.SingleCoffeeActivity"));
+
+        // Press the back button
+        pressBack();
+
+        // Check again that the Cafe Bombon item is displayed
+        onView(withText("Cafe Bombon")).check(matches(isDisplayed()));
+
+    }
+
 }
